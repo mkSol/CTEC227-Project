e0b447711124ea8d9c1bdea2ac333bd4ae799d57
mkSol <mdk@kles.com>
2014-05-15 04:24:02 -0700

    Almost full website skeleton complete

--- helpdesk/viewallequip.php
+++ helpdesk/viewallequip.php
@@ -10,27 +10,25 @@
 
 	echo "<table>";
 	echo "<tr>";
-	echo "<th>Ticket ID</th>";
+	echo "<th>Equip ID</th>";
+	echo "<th>Type</th>";
+	echo "<th>Description</th>";
+	echo "<th>User ID</th>";
+	echo "<th>Username</th>";
 	echo "<th>First</th>";
 	echo "<th>Last</th>";
-	echo "<th>Username</th>";
-	echo "<th>Date</th>";
-	echo "<th>Category</th>";
-	echo "<th>Priority</th>";
-	echo "<th>Status</th>";
-	echo "<th>Description</th>";
+	echo "<th>Department</th>";
 	echo "</tr>";
 	while ($rows=mysqli_fetch_array($result)) {
 		echo "<tr>";
-		echo "<td>" . $rows['ticketID'] . "</td>";
+		echo "<td>" . $rows['equipID'] . "</td>";
+		echo "<td>" . $rows['equipType'] . "</td>";
+		echo "<td>" . $rows['equipDesc'] . "</td>";
+		echo "<td>" . $rows['userID'] . "</td>";
+		echo "<td>" . $rows['username'] . "</td>";
 		echo "<td>" . $rows['firstName'] . "</td>";
 		echo "<td>" . $rows['lastName'] . "</td>";
-		echo "<td>" . $rows['username'] . "</td>";
-		echo "<td>" . $rows['timestamp'] . "</td>";
-		echo "<td>" . $rows['category'] . "</td>";
-		echo "<td>" . $rows['priority'] . "</td>";
-		echo "<td>" . $rows['status'] . "</td>";
-		echo "<td>" . $rows['issueDesc'] . "</td>";
+		echo "<td>" . $rows['department'] . "</td>";
 		echo "</tr>";
 	}
 	echo "</table>";
