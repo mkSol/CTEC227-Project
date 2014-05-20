886506c0d01e91e06af7fe87f43fee985bfdbbbb
Robert Barnes <patrick_barnes@r.barnes5>
2014-05-20 02:09:09 -0700

    oops
    fixing a small error I had made with a previous commit

--- helpdesk/dashboard.php
+++ helpdesk/dashboard.php
@@ -86,7 +86,7 @@
   			<section class="top-bar-section">
 			<ul class="right">
 				<li><a class="myTickets" href="#">View My Tickets</a></li>
-				<li><a class="newTicket" href="#newTicket">Create New Ticket</a></li>
+				<li><a class="newTicket" href="#">Create New Ticket</a></li>
 				<li><a class="myEquip" href="#">View My Equipment</a></li>
 				<li><a class="allTickets" href="#">View Ticket Pool</a></li>
 				<li><a class="assignedTickets" href="#">View Assigned Tickets</a></li>
