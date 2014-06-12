847cc4a062f1c1811157af5a17e1bc91900d6f37
mkSol <mdk@kles.com>
2014-06-12 04:49:51 -0700

    Close alert additions

--- helpdesk/incl/alerts.inc.php
+++ helpdesk/incl/alerts.inc.php
@@ -6,60 +6,70 @@ if (isset($_POST['submitType'])) {
 			case 'newPass':
 				echo '<div data-alert class="alert-box success radius">';
 				echo "Your password has been updated.";
+				echo '<a href="#" class="close">&times;</a>';
 				echo '</div>';
 				break;
 
 			case 'newMessage':
 				echo '<div data-alert class="alert-box success radius">';
 				echo "Your message has been sent.";
+				echo '<a href="#" class="close">&times;</a>';
 				echo '</div>';
 				break;
 
 			case 'newTicket':
 				echo '<div data-alert class="alert-box success radius">';
 				echo "Your ticket has been submitted.";
+				echo '<a href="#" class="close">&times;</a>';
 				echo '</div>';
 				break;
 
 			case 'editDynamic':
 				echo '<div data-alert class="alert-box success radius">';
 				echo "Record has been updated.";
+				echo '<a href="#" class="close">&times;</a>';
 				echo '</div>';
 				break;
 			
 			case 'edit':
 				echo '<div data-alert class="alert-box success radius">';
 				echo "Ticket has been updated.";
+				echo '<a href="#" class="close">&times;</a>';
 				echo '</div>';
 				break;
 
 			case 'comment':
 				echo '<div data-alert class="alert-box success radius">';
 				echo "Your comment has been submitted.";
+				echo '<a href="#" class="close">&times;</a>';
 				echo '</div>';
 				break;
 
 			case 'deleteDynamic':
 				echo '<div data-alert class="alert-box success radius">';
 				echo "Record has been deleted.";
+				echo '<a href="#" class="close">&times;</a>';
 				echo '</div>';
 				break;
 
 			case 'delete':
 				echo '<div data-alert class="alert-box success radius">';
 				echo "Ticket has been deleted.";
+				echo '<a href="#" class="close">&times;</a>';
 				echo '</div>';
 				break;
 
 			case 'editEquip':
 				echo '<div data-alert class="alert-box success radius">';
 				echo "Equipment has been updated.";
+				echo '<a href="#" class="close">&times;</a>';
 				echo '</div>';
 				break;
 
 			case 'assign':
 				echo '<div data-alert class="alert-box success radius">';
 				echo "Ticket has been assigned to you.";
+				echo '<a href="#" class="close">&times;</a>';
 				echo '</div>';
 				break;
 			
